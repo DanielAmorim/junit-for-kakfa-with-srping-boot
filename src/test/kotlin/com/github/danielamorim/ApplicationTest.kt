98289261dfdf993b31ffa465287afd25795cc46b
Daniel Amorim <daniel.amorim@farfetch.com>
2020-04-10 22:00:25 +0100

    feat: Test EmbeddedKafkaCluster initialization

--- src/test/kotlin/com/github/danielamorim/ApplicationTest.kt
+++ src/test/kotlin/com/github/danielamorim/ApplicationTest.kt
@@ -1,12 +1,22 @@
 package com.github.danielamorim
 
+import net.mguenther.kafka.junit.EmbeddedKafkaCluster
+import net.mguenther.kafka.junit.EmbeddedKafkaClusterConfig
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ApplicationTest {
+
     @Test
     fun `should hello() return "Hello, World"`() {
         assert(hello() == "Hello, World!")
+    }
+
+    @Test
+    fun `should embeddedKafkaCluster different of null`() {
+        val embeddedKafkaCluster = EmbeddedKafkaCluster.provisionWith(EmbeddedKafkaClusterConfig.useDefaults())
+
+        assert(embeddedKafkaCluster != null)
     }
 }
